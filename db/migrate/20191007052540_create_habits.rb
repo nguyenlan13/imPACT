19a5826a6db3edae9ae51a446b6e9ef143938a6b
lanibon <nguyenlan13@gmail.com>
2019-10-06 23:12:49 -0700

    add table attributes

--- db/migrate/20191007052540_create_habits.rb
+++ db/migrate/20191007052540_create_habits.rb
@@ -1,6 +1,9 @@
 class CreateHabits < ActiveRecord::Migration[6.0]
   def change
     create_table :habits do |t|
+      t.string :title
+      t.string :description
+      
 
       t.timestamps
     end
