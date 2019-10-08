a8549754a30c8630ae7abc1a3ed1398ddc77d55a
lanibon <nguyenlan13@gmail.com>
2019-10-07 18:45:55 -0700

    add activerecord association for user_teams

--- app/models/pact.rb
+++ app/models/pact.rb
@@ -1,5 +1,5 @@
 class Pact < ApplicationRecord
-  has_many :users, through: :user_teams
-  has_many :user_teams
-  
+  has_many :user_pacts
+  has_many :users, through: :user_pacts
+
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -2,7 +2,8 @@ class User < ApplicationRecord
   has_secure_password
   has_many :habits
   has_many :identities, through: :habits
-  has_many :pacts, through: :user_teams
+  has_many :user_pacts
+  has_many :pacts, through: :user_pacts
 
 
 end
