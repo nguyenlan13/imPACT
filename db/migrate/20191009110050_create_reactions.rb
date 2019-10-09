9d2b2d12785f15841657f329e49833450b2d66ca
lanibon <nguyenlan13@gmail.com>
2019-10-09 05:34:19 -0700

    add attributes for comment and reaction tables

--- db/migrate/20191009110050_create_reactions.rb
+++ db/migrate/20191009110050_create_reactions.rb
@@ -1,7 +1,9 @@
 class CreateReactions < ActiveRecord::Migration[6.0]
   def change
     create_table :reactions do |t|
-
+      t.integer :user_id
+      t.references :reactable, polymorphic: true, index: true
+      t.integer :reaction_type
       t.timestamps
     end
   end
