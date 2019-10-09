e5c73c840cff2a5dfe4aaea0cae324c000e28f54
lanibon <nguyenlan13@gmail.com>
2019-10-09 05:33:46 -0700

    add validations and associations for polymorphic models

--- app/models/comment.rb
+++ app/models/comment.rb
@@ -1,2 +1,12 @@
 class Comment < ApplicationRecord
+  validates :user_id, presence: true
+  validates :content, presence: true
+  validates :commentable_id, presence: true
+  validates :commentable_type, presence: true
+
+  belongs_to :user
+  belongs_to :commentable, polymorphic: true
+
+  has_many :comments, as: :commentable
+  has_many :reactions, as: :reactable
 end
