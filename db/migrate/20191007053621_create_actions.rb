81a19cd34f4bdcf18162d8a2e51e78b16f33613f
lanibon <nguyenlan13@gmail.com>
2019-10-09 06:02:17 -0700

    minor change to actions table

--- db/migrate/20191007053621_create_actions.rb
+++ db/migrate/20191007053621_create_actions.rb
@@ -3,10 +3,9 @@ class CreateActions < ActiveRecord::Migration[6.0]
     create_table :actions do |t|
       t.integer :habit_id
       t.string :action_step
-      t.string :location
       t.string :start_datetime
+      t.string :location
       t.string :duration
-
       t.timestamps
     end
   end
