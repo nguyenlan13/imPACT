46323a48659ff0766bcee8aa9dcdc2a204359f83
lanibon <nguyenlan13@gmail.com>
2019-10-10 05:00:34 -0700

    add helper methods for user sign up

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,14 @@
 class ApplicationController < ActionController::Base
+    helper_method :logged_in?
+    helper_method :current_user
+
+    private
+
+    def logged_in?
+        !!current_user
+    end
+    
+    def current_user
+        User.find_by(id: session[:user_id])
+    end
 end
