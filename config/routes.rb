f90608ae343a762246618a371d321723109f76be
lanibon <nguyenlan13@gmail.com>
2019-10-10 05:02:05 -0700

    add signup and logout routes

--- config/routes.rb
+++ config/routes.rb
@@ -6,6 +6,9 @@ Rails.application.routes.draw do
   resources :actions
   resources :identities
   resources :habits
-  resources :users
+  resources :users, except: [:new]
+
+  get "/signup" => "users#new", as: "signup"
+  delete "/logout" => "sessions#destroy"
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
