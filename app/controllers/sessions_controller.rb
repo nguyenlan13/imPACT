3220b4a777589548536d85610a3fcef1b3f65565
lanibon <nguyenlan13@gmail.com>
2019-10-10 05:35:01 -0700

    add actions for sessions

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -1,10 +1,23 @@
 class SessionsController < ApplicationController
 
-  def login
+    def new
 
-  end
+    end
 
-  def logout
+    def create
+        @user = User.find_by(username: params[:username])
+        if @user && @user.authenticate(params[:password])
+            session[:user_id] = @user.id
+            flash [:success] = "Welcome, #{@user.username}"
+            # redirect_to identities_path?
+        else
+            flash[:danger] = "Login credentials were not found, please try again."
+            redirect_to login_path
+        end
+    end
 
-  end
+    def destroy
+        session.delete
+        redirect_to login_path
+    end
 end
