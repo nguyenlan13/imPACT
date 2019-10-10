ae2b5d0ad018526f99d1fa21f9b3a75d592e7cce
lanibon <nguyenlan13@gmail.com>
2019-10-10 05:34:19 -0700

    add helper methods for authentication

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -7,8 +7,16 @@ class ApplicationController < ActionController::Base
     def logged_in?
         !!current_user
     end
-    
+
     def current_user
         User.find_by(id: session[:user_id])
+    end
+
+    def log_in(user)
+        session[:user_id] = user.id
+    end
+
+    def authenticate
+        redirect_to login_path if !logged_in?
     end
 end
