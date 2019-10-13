8cb3cf1f756e77288b6eb806fa24be8129d123b3
lanibon <nguyenlan13@gmail.com>
2019-10-13 14:17:47 -0700

    add nested routes for user

--- config/routes.rb
+++ config/routes.rb
@@ -1,14 +1,20 @@
 Rails.application.routes.draw do
-  resources :reactions
-  resources :comments
-  resources :streaks
-  resources :pacts
-  resources :actions
-  resources :identities
-  resources :habits
-  resources :users, except: [:new]
+    resources :reactions
+    resources :comments
+    resources :streaks
+    resources :pacts
+    resources :actions
+    resources :identities
+    resources :habits
+    resources :users, except: [:new]
+    
+    resources :users, only: [:show] do  
+        resources :identities, only: [:index, :new, :create]
+    end
 
-  get "/signup" => "users#new", as: "signup"
-  delete "/logout" => "sessions#destroy"
+    get "/signup" => "users#new", as: "signup"
+    get "/login" => "sessions#new", as: "login"
+    post "/login" => "sessions#create"
+    delete "/logout" => "sessions#destroy"
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
