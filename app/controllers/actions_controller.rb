83208f01d44d9090c9ea777ec9fd6a70f20cfe4b
lanibon <nguyenlan13@gmail.com>
2019-10-15 18:32:15 -0700

    add action functionality

--- app/controllers/actions_controller.rb
+++ app/controllers/actions_controller.rb
@@ -1,16 +1,41 @@
 class ActionsController < ApplicationController
+    def index
+        @actions = Action.all
+    end
 
     def new
-
+        @action = Action.new
     end
 
     def create
+        if params[:user_id]
+            @user = User.find(id: params[:user_id])
+            @action = @user.actions.build(action_params)
+            if @action.save!
+                redirect_to user_path(@user)
+            else 
+               redirect_to new_action_path
+            end
+        else
+            @action = Action.new(action_params)
+            if @action.save 
+                redirect_to action_path(@action) 
+            else
+                redirect_to new_action_path
+            end
+        end 
     end
-    
-    def update
+
+    # def edit
+
+    # end
+
+    def show
+        @action = Action.find(params[:id])
     end
-    
-    def destroy
-    end
+
+    def action_params
+        params.require(:action).permit(:user_id, :habit_id, :action_step, :start_datetime, :location, :duration)
+      end
 
 end
--- app/models/action.rb
+++ app/models/action.rb
@@ -1,4 +1,5 @@
 class Action < ApplicationRecord
   belongs_to :user
+  belongs_to :habit
 
 end
