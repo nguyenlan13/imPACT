bc9f1e6f51424f6de11a95bb7d1b627d70de3bef
lanibon <nguyenlan13@gmail.com>
2019-10-16 07:14:35 -0700

    add google omniauth functionality

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,12 +1,19 @@
 class User < ApplicationRecord
-  validates :username, uniqueness: true
+    has_secure_password
+    validates :username, uniqueness: true
+    has_many :user_identities
+    has_many :identities, through: :user_identities
+    has_many :habits, through: :identities
+    has_many :actions
 
-  has_secure_password
 
-  has_many :user_identities
-  has_many :identities, through: :user_identities
-  has_many :habits, through: :identities
-  has_many :actions
+  def self.from_omniauth(auth)
+    # Creates a new user only if it doesn't exist
+    where(email: auth.info.email).first_or_initialize do |user|
+      user.name = auth.info.name
+      user.email = auth.info.email
+    end
+  end
 #   has_many :comments, as: :commentable
 
 #   accepts_nested_attributes_for :identities
--- db/migrate/20191007052316_create_users.rb
+++ db/migrate/20191007052316_create_users.rb
@@ -1,6 +1,8 @@
 class CreateUsers < ActiveRecord::Migration[6.0]
   def change
     create_table :users do |t|
+        t.string :google_token
+        t.string :google_refresh_token
         t.string :name
         t.string :username
         t.string :password_digest
