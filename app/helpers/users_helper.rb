c704b8721b054a0c1f5b0a07cf1d9f6598bc7a71
lanibon <nguyenlan13@gmail.com>
2019-10-20 14:41:10 -0700

    add helper method to users

--- app/helpers/users_helper.rb
+++ app/helpers/users_helper.rb
@@ -1,2 +1,10 @@
 module UsersHelper
+
+    def add_identity
+        if params[:identities]
+            identity.each do |k,v|
+            @user.identities << Identity.find(k)
+            end
+         end
+    end
 end
