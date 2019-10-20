ada46719ff7016ea8ba6d9bd6992b96bf4a469fe
lanibon <nguyenlan13@gmail.com>
2019-10-20 14:40:39 -0700

    add method to streak model

--- app/models/streak.rb
+++ app/models/streak.rb
@@ -5,12 +5,12 @@ class Streak < ApplicationRecord
   has_many :reactions, as: :reactable, dependent: :destroy
 
 
-def less_than_a_day(habit1, habit2)
-    counter = 0
-    if (habit1.created_at - habit2.created_at)  < (idk how to get 24 hours with Time)
-        counter += 1
-    end
-end
+# def less_than_a_day(habit1, habit2)
+#     counter = 0
+#     if (habit1.created_at - habit2.created_at)  < (idk how to get 24 hours with Time)
+#         counter += 1
+#     end
+# end
 
 
 end
