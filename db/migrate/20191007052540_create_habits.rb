b0ecbdd4ee9212f1f5e9f4b18811ec036e9a8190
lanibon <nguyenlan13@gmail.com>
2019-10-21 00:11:59 -0700

    change habit attributes and partial

--- db/migrate/20191007052540_create_habits.rb
+++ db/migrate/20191007052540_create_habits.rb
@@ -1,8 +1,9 @@
 class CreateHabits < ActiveRecord::Migration[6.0]
   def change
     create_table :habits do |t|
+        t.boolean :build, default: true
         t.string :title
-        t.text :description
+        t.integer :frequency_number
         t.string :frequency
         t.timestamps
     end
