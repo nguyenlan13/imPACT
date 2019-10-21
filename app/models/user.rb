bec2c3e6aeafc630a382ed68958240479c066c35
lanibon <nguyenlan13@gmail.com>
2019-10-21 00:09:31 -0700

    change user assocation with streaks, form, and profile

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,15 +1,21 @@
 class User < ApplicationRecord
- 
     validates :username, presence: true, uniqueness: true
     validates :name, presence: true
     validates :email, presence: true, uniqueness: true
-    validates :password, length: { within: 8..21 }, on: :create
+    validates :password, presence: true, length: { within: 8..21 }, on: :create
     has_secure_password
+
+
 
     has_many :user_identities, dependent: :destroy
     has_many :identities, through: :user_identities
     has_many :habits, through: :identities
     has_many :actions, dependent: :destroy
+    has_many :streaks, dependent: :destroy
+    has_many :comments, dependent: :destroy
+    has_many :reactions, dependent: :destroy
+
+
 
     def self.g_random_code(number)
         array = Array('A'..'Z') + Array('a'..'z')
