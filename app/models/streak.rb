8064cbc11728a9bdf9571a1e82c554aa9f3572aa
lanibon <nguyenlan13@gmail.com>
2019-10-21 00:10:23 -0700

    change streaks associations and remigrate

--- app/models/streak.rb
+++ app/models/streak.rb
@@ -1,5 +1,6 @@
 class Streak < ApplicationRecord
   belongs_to :habit
+  belongs_to :user
 
   has_many :comments, as: :commentable, dependent: :destroy
   has_many :reactions, as: :reactable, dependent: :destroy
--- db/migrate/20191008023055_create_streaks.rb
+++ db/migrate/20191008023055_create_streaks.rb
@@ -2,6 +2,7 @@ class CreateStreaks < ActiveRecord::Migration[6.0]
   def change
     create_table :streaks do |t|
         t.integer :habit_id
+        t.integer :user_id
         t.integer :current_streak
         t.integer :longest_streak
         t.datetime :longest_streak_end_date
