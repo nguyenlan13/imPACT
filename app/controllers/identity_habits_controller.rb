8ebaea3884b8021f079aedae5d7e5ad905d3f3d9
lanibon <nguyenlan13@gmail.com>
2019-10-27 19:46:44 -0700

    add scope to prevent duplicate joins

--- app/controllers/identity_habits_controller.rb
+++ app/controllers/identity_habits_controller.rb
@@ -1,15 +1,14 @@
 class IdentityHabitsController < ApplicationController
 
     def create
-        # byebug
         identity_habit = IdentityHabit.new(identity: Identity.find(params[:identity_habit][:identity_id]), habit: Habit.find(params[:habit_id]))
-
         if identity_habit.save
-
-            redirect_to identity_habits_path(identity_habit)
+            redirect_to(request.env['HTTP_REFERER'])
+            # redirect_to identity_habits_path(identity_habit)
         else
             flash[:danger] = "Habit was not linked to identity, please try again"
-            redirect_to habit_path(params[:habit_id])
+            redirect_to(request.env['HTTP_REFERER'])
+            # redirect_to habit_path(params[:habit_id])
         end
     end
 
--- app/models/user_identity.rb
+++ app/models/user_identity.rb
@@ -1,4 +1,6 @@
 class UserIdentity < ApplicationRecord
     belongs_to :user
     belongs_to :identity
+    validates_uniqueness_of :user_id, :scope => :identity_id
+
 end
