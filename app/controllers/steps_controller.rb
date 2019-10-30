af14d40c9d6bbac0def14865346e7a0b81565884
lanibon <nguyenlan13@gmail.com>
2019-10-29 19:35:11 -0700

    attempt to add edit and update functionality

--- app/controllers/steps_controller.rb
+++ app/controllers/steps_controller.rb
@@ -6,7 +6,12 @@ class StepsController < ApplicationController
     def index
         @user = current_user
         if params[:habit_id]
+            # @habit = Habit.find(params[:habit_id])
+            # @steps = @habit.steps
             @steps = Habit.find(params[:habit_id]).steps
+            # @step.user = current_user
+        # else
+        #     @steps = Step.all
         end
     end
 
@@ -22,32 +27,57 @@ class StepsController < ApplicationController
             @step = @habit.steps.build(step_params)
             @step.user = current_user
             if @step.save
-                redirect_to habit_steps_path(@habit)
-            else 
-                redirect_to new_step_path
-            end
-        else
-            @step = Step.new(step_params)
-            if @step.save 
-                redirect_to step_path(@step) 
+                redirect_to user_steps_path(current_user)
+                # redirect_to habit_steps_path(@habit)
             else
+                flash[:danger] = "Action was not saved, please try again."
                 render :new
-                # redirect_to new_action_path
             end
+        # else
+        #     @step = Step.new(step_params)
+        #     if @step.save 
+        #         redirect_to step_path(@step) 
+        #     else
+        #         render :new
+        #         # redirect_to new_action_path
+        #     end
         end 
     end
 
     def edit
-
+        @step = Step.find(params[:id])
+        render :edit
+        # if params[:habit_id]
+        # @user = current_user  
+        # @habit = Habit.find(params[:habit_id])
+        # # # @step = @habit.steps.build
+        # @step = @habit.steps.find(params[:id])
+        # @user = current_user  
+        # @habit = Habit.find(params[:habit_id])
+        # @step = @habit.steps.build
+        # end
     end
 
     def update
         @step.update(step_params)
-        redirect_to habit_steps_path(@habit)
+        byebug
+        if @step.save
+            redirect_to user_steps_path(current_user)
+            # redirect_to habit_steps_path(@habit)
+        else
+            flash[:danger] = "Action was not updated, please try again."
+            render :edit
+        end
+        # redirect_to habit_steps_path(@habit)
     end
 
     def show
         @step = Step.find(params[:id])
+    end
+
+    def destroy
+        @step = Step.find(params[:id])
+        @step.delete
     end
 
     private
