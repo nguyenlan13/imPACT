b3b3cb2f9ac90f53e4312284dcd738c727cc8668
lanibon <nguyenlan13@gmail.com>
2019-10-29 19:27:32 -0700

    change redirect paths in create

--- app/controllers/identity_habits_controller.rb
+++ app/controllers/identity_habits_controller.rb
@@ -6,19 +6,17 @@ class IdentityHabitsController < ApplicationController
             redirect_to(request.env['HTTP_REFERER'])
             # redirect_to identity_habits_path(identity_habit)
         else
-            flash[:danger] = "Habit was not linked to identity, please try again"
+            flash[:danger] = "This habit is already linked to this identity."
             redirect_to(request.env['HTTP_REFERER'])
             # redirect_to habit_path(params[:habit_id])
         end
     end
 
     def destroy
-        # identity_habit = IdentityHabit.new(identity: params[:identity_id], habit: Habit.find(params[:habit_id]))
-        # if identity_habit.save
-        #     redirect_to identity_habits_path(@identity.id)
-        # else
-        #     flash[:danger] = "Habit was not saved, please try again"
-        #     redirect_to habit_path(params[:habit_id])
-        # end
+        IdentityHabit.find(params[:id]).destroy
+        # identity = Identity.find(params[:id]
+        # # identity.identity_habits.find(params[:identity_habit_id]).destroy
+        # IdentityHabit.find(identity.id, habit.id).destroy
+        redirect_to(request.env['HTTP_REFERER'])
     end
 end
--- app/controllers/user_identities_controller.rb
+++ app/controllers/user_identities_controller.rb
@@ -1,24 +1,18 @@
 class UserIdentitiesController < ApplicationController
 
     def create
-        # if UserIdentity.find(user: current_user, identity: Identity.find(params[:identity_id]))
-        #     # already exists
-        #     flash[:danger] = "You've already joined this PACT."
-        #     redirect_to identity_path(params[:identity_id])
-        #     return
-        # end
-        
         user_identity = UserIdentity.new(user: current_user, identity: Identity.find(params[:identity_id]))
         if user_identity.save
-            redirect_to user_identities_path(current_user)
-        else
-            flash[:danger] = "Identity was not saved, please try again"
             redirect_to identity_path(params[:identity_id])
+        else
+            flash[:danger] = "You've already joined this PACT."
+            redirect_to(request.env['HTTP_REFERER'])
         end
     end
 
     def destroy
-        
+        user_identity = UserIdentity.find(params[:id])
+        user_identity.delete
+        redirect_to(request.env['HTTP_REFERER'])
     end
-
 end
