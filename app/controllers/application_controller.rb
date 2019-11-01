768a3d2737e61335bf0cfeb49a2a2ae6d660dd8a
lanibon <nguyenlan13@gmail.com>
2019-11-01 02:50:00 -0700

    add authorization and helper methods

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,8 +2,8 @@ class ApplicationController < ActionController::Base
     helper_method :logged_in?
     helper_method :current_user
     helper_method :owner?
-
-
+    helper_method :authorize_to_view
+ 
 
     rescue_from ActiveRecord::RecordNotFound, :with => :rescue404
     rescue_from ActionController::RoutingError, :with => :rescue404
@@ -25,22 +25,16 @@ class ApplicationController < ActionController::Base
     end
 
     def authorize(resource)
+        raise Errors::AuthorizationError.new if !resource
         raise Errors::AuthorizationError.new if !owner?(resource)
     end
 
-    # def authorize
-    #     if !logged_in? || current_user.nil?
-    #         redirect '/login'
-    #     end
-    #   end
-
-
-    # def auth_edit(resource)
-    #     authorize
-    #     raise Errors::AuthorizationError.new if !resource
-    #     raise Errors::AuthorizationError.new if !owner?(resource)
-    # end
-
+    def authorize_to_view(path)
+        unless params[:user_id].to_i == current_user.id
+        flash[:danger] = "You are not authorized to see this page."
+        redirect_to path
+        end
+    end
 
     def log_in(user)
         session[:user_id] = user.id
