c1e7546f355fe69aa4d451e2ca0a40592903ede6
lanibon <nguyenlan13@gmail.com>
2019-11-01 02:48:44 -0700

    add validations and scope method

--- app/models/habit.rb
+++ app/models/habit.rb
@@ -1,4 +1,11 @@
 class Habit < ApplicationRecord
+    # validates :build, presence: true
+    validates :title, presence: true
+    validates :frequency_number, presence: true, numericality: true
+    validates :frequency, presence: true
+    
+    
+    
     has_many :streaks, dependent: :destroy
     has_many :steps, dependent: :destroy
     has_many :identity_habits, dependent: :destroy
@@ -6,8 +13,15 @@ class Habit < ApplicationRecord
     has_many :users, through: :identities
     has_many :comments, as: :commentable, dependent: :destroy
 
-#    accepts_nested_attributes_for :actions
-
     #scope method
+
+    def self.building
+        where(build: true).order(created_at: :desc).limit(5)
+    end
+
+    def self.quit
+        where(build: false).order(created_at: :desc).limit(5)
+    end
+
 end
 
